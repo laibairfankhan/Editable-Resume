e375df4b3a0f255b8a44cfb8946988c9e53ae7d3
laibairfankhan <irfanlaiba978@gmail.com>
2024-12-18 17:45:56 +0500

    Your commit message here

---
+++ Typescript.js
@@ -0,0 +1,23 @@
+// get refernces to the form and display area
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+// handel form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // collect input value
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //generate the resume  dynamically
+    var resumeHTML = "\n    <h2><b>Resume</h2>\n     <h3Personal Information</h3>\n      <p><b>Name:</b>".concat(name, "</p>\n        <p><b>Email:</b>").concat(email, "</p>\n          <p><b>Phone:</b>").concat(phone, "</p>\n\n          <h3>Education</h3>\n          <p>").concat(education, "</p>\n          \n          <h3>Education</h3>\n          <p>").concat(experience, "</p>\n          \n          <h3>Skills</h3>\n          <p>").concat(skills, "</p>\n          ");
+    //   display generate resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing.');
+    }
+});
